9cfbb72510c61452b01a38b9ab2b42f7044d591d
Kowies <kowieski.m98@gmail.com>
2019-03-08 19:16:19 +0100

    Change text formatting of Problem 019

--- Problem 019/script.py
+++ Problem 019/script.py
@@ -11,10 +11,9 @@ end_date = datetime.date(year=2001, month=1,day=1)
 
 one_month = relativedelta(months=1)
 
+
 def main():
-
     temp_date = begin_date
-
     how_many_sundays = 0
 
     while temp_date < end_date:
